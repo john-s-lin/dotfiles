9ee4e4d69e0128d7ae6e276284cf062c5cc19b5e
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2023-02-21 18:31:20 -0500

    feat: update install script to brew from brewfile

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Install Xcode CLT
+xcode-select --install
+
 # Install Homebrew
 echo "Checking for Homebrew..."
 if test ! "$(which brew)"; then
@@ -10,112 +13,14 @@ else
     echo "$(brew --version) already installed."
 fi
 
-# Install git
-echo "Checking for git..."
-if test ! "$(which git)"; then
-    echo "Installing git..."
-    brew install git
-    echo "git installed!"
-else
-    echo "$(git --version) already installed."
-fi
+# Check brew doctor
+brew doctor
 
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# Install coreutils for passion.zsh-theme
-echo "Installing coreutils..."
-brew install coreutils
-echo "coreutils installed!"
-
-# Install openjdk@17
-echo "Installing openjdk@17..."
-brew install openjdk@17
-echo "openjdk@17 installed!"
-
-# Install nvm
-echo "Installing nvm..."
-brew install nvm
-echo "nvm installed!"
-
-# Install tree
-echo "Installing tree..."
-brew install tree
-echo "tree installed!"
-
-# Install neovim
-echo "Installing neovim..."
-brew install neovim
-echo "neovim installed!"
-
-##########
-# CASKS
-##########
-
-# Install Firefox
-echo "Installing firefox..."
-brew install --cask firefox
-echo "firefox installed!"
-
-# Install iTerm2
-echo "Installing iterm2..."
-brew install --cask iterm2
-echo "iterm2 installed!"
-
-# Install chrome
-echo "Installing google-chrome..."
-brew install --cask google-chrome
-echo "google-chrome installed!"
-
-# Install drive
-echo "Installing google-drive..."
-brew install --cask google-drive
-echo "google-drive installed!"
-
-# Install spotify
-echo "Installing spotify..."
-brew install --cask spotify
-echo "spotify installed!"
-
-# Install miniconda
-echo "Installing miniconda..."
-brew install --cask miniconda
-echo "miniconda installed!"
-
-# Install rectangle
-echo "Installing rectangle..."
-brew install --cask rectangle
-echo "rectangle installed!"
-
-# Install selfcontrol
-echo "Installing selfcontrol..."
-brew install --cask selfcontrol
-echo "selfcontrol installed!"
-
-# Install vlc
-echo "Installing vlc..."
-brew install --cask vlc
-echo "vlc installed!"
-
-# Install vscode
-echo "Installing vscode..."
-brew install --cask visual-studio-code
-echo "vscode installed!"
-
-# Install microsoft-office
-echo "Installing office..."
-brew install --cask microsoft-office
-echo "office installed!"
-
-# Install discord
-echo "Installing discord..."
-brew install --cask discord
-echo "discord installed!"
-
-# Install tomatobar
-echo "Installing tomatobar..."
-brew install --cask tomatobar
-echo "tomatobar installed!"
+# Install brew packages from Brewfile
+brew bundle install --file="$(pwd)"/Brewfile
 
 # Initialize miniconda
 conda init "$(basename "${SHELL}")"
