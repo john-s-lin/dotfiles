de78132ccd34fb909d48ac9895a0aee66f2649d7
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2023-02-27 00:52:53 -0500

    refactor: download passion to custom themes folder

--- scripts/zsh_setup.sh
+++ scripts/zsh_setup.sh
@@ -4,7 +4,7 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Download passion.zsh-theme
-sh -c "$(curl -o https://raw.githubusercontent.com/ChesterYue/ohmyzsh-theme-passion/master/passion.zsh-theme ~/.oh-my-zsh/themes/)"
+sh -c "$(curl -o https://raw.githubusercontent.com/ChesterYue/ohmyzsh-theme-passion/master/passion.zsh-theme "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/themes/)"
 
 # Download zsh-autosuggestions plugin
 git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
