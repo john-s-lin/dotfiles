481c73ec44a5706c505d554c5242254a130d7f6f
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2023-09-05 14:37:06 -0400

    patch: update tmux install script

--- scripts/tmux_setup.sh
+++ scripts/tmux_setup.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # Install oh-my-tmux
-git clone https://github.com/gpakosz/.tmux.git "~/.oh-my-tmux"
-mkdir -p "~/.config/tmux"
+git clone https://github.com/gpakosz/.tmux.git "$HOME/.oh-my-tmux"
+mkdir -p "$HOME/.config/tmux"
 
 # Symlink oh-my-tmux config
-ln -s "~/.oh-my-tmux/.tmux.conf" "~/.config/tmux/tmux.conf"
+ln -s "$HOME/.oh-my-tmux/.tmux.conf" "$HOME/.config/tmux/tmux.conf"
 
 exit 0
 
