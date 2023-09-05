15b46a0805b77f70e3a8230ac26e2210e2fcbe27
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2023-09-05 14:49:42 -0400

    patch: add symlink for tmux config local

--- scripts/tmux_setup.sh
+++ scripts/tmux_setup.sh
@@ -7,5 +7,8 @@ mkdir -p "$HOME/.config/tmux"
 # Symlink oh-my-tmux config
 ln -s "$HOME/.oh-my-tmux/.tmux.conf" "$HOME/.config/tmux/tmux.conf"
 
+# Symlink .dotfiles/config/tmux.conf.local
+ln -s "$HOME/.dotfiles/config/tmux.conf.local" "$HOME/.config/tmux/tmux.conf.local"
+
 exit 0
 
