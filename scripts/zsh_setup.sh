b8d6e8f8a35671478ab90ce204dab73f35e90bab
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2023-09-27 16:42:55 -0400

    feat: modified zsh setup script

--- scripts/zsh_setup.sh
+++ scripts/zsh_setup.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 
+# Assert ZSH_CUSTOM exists
+if [ -z "${ZSH_CUSTOM}" ]; then
+  ZSH_CUSTOM="${HOME}/.oh-my-zsh/custom"
+fi
+
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# Download passion.zsh-theme
-sh -c "$(curl https://raw.githubusercontent.com/ChesterYue/ohmyzsh-theme-passion/master/passion.zsh-theme -O --output-dir "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/themes)"
+# Download modified passion.zsh-theme from saved config
+cp "${HOME}/.dotfiles/config/zsh/themes/passion.zsh-theme" "${ZSH_CUSTOM}"/themes/
 
 # Download zsh-autosuggestions plugin
-git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM}"/plugins/zsh-autosuggestions
 
 # Download zsh-syntax-highlighting plugin
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM}"/plugins/zsh-syntax-highlighting
 
 exit 0
