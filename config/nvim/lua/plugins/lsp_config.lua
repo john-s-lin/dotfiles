b81a083bf996b6d683731dd0ed14e53eda397bf1
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2023-10-03 10:09:45 -0400

    feat: update lsp_config with none-ls

--- config/nvim/lua/plugins/lsp_config.lua
+++ config/nvim/lua/plugins/lsp_config.lua
@@ -1,15 +1,15 @@
-local null_ls = require("null-ls")
+local none_ls = require("none-ls")
 
 local opts = {
   sources = {
-    null_ls.builtins.diagnostics.mypy,
-    null_ls.builtins.diagnostics.ruff,
+    none_ls.builtins.diagnostics.mypy,
+    none_ls.builtins.diagnostics.ruff,
   },
 }
 
 local plugins = {
   {
-    "jose-elias-alvarez/null-ls.nvim",
+    "nvimtools/none-ls.nvim",
     ft = {
       "python",
     },
