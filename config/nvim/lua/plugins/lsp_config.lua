86c80b4e8037d401b9782ce7f9882a5edbaf60d0
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2023-10-03 10:22:43 -0400

    patch: none-ls rename as null-ls
    Because to ensure compatibility, the maintainers forced none-ls to retain null-ls naming

--- config/nvim/lua/plugins/lsp_config.lua
+++ config/nvim/lua/plugins/lsp_config.lua
@@ -1,4 +1,7 @@
-local none_ls = require("none-ls")
+-- Eventually when null-ls is fully deprecated, you may have to rename
+-- require("null-ls") => require("none-ls")
+-- Ref: https://github.com/LazyVim/LazyVim/blob/7cac6f064c5b30f6b0ae4dffab62100aa6fe2568/lua/lazyvim/config/init.lua#L83
+local none_ls = require("null-ls")
 
 local opts = {
   sources = {
