559bbdc68699f8bb86686653b893bc7edda76539
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2023-11-15 21:09:20 -0500

    feat: add linux support (#8)
    * feat: install.sh defers to OS-specific install scripts
    
    * feat: add macos install script
    
    * feat: init linux install script

--- install.sh
+++ install.sh
@@ -2,51 +2,29 @@
 
 # Ensure you are running as root
 if [[ ${EUID} = 0 ]]; then
-  echo "(1) Running as superuser already."
+	echo "(1) Running as superuser already."
 else
-  # Reset user as superuser
-  sudo -k
-  if sudo true; then
-    echo "(2) User changed to sudo."
-  else
-    echo "(3) Wrong password."
-    exit 1
-  fi
+	# Reset user as superuser
+	sudo -k
+	if sudo true; then
+		echo "(2) User changed to sudo."
+	else
+		echo "(3) Wrong password."
+		exit 1
+	fi
 fi
 
-# Install Xcode CLT
-xcode-select --install
+OS="$(uname -s)"
 
-# Install Homebrew
-echo "Checking for Homebrew..."
-if test ! "$(which brew)"; then
-    echo "Installing Homebrew..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    echo "Homebrew installed!"
+if [[ "$OS" == "Darwin" ]]; then
+	echo "Running install script for MacOS..."
+	source install_macos.sh
+elif [[ "$OS" == "Linux" ]]; then
+	echo "Running install script for Linux..."
+	source install_linux.sh
 else
-    echo "$(brew --version) already installed."
+	echo "Not a supported OS. Exiting..."
+	exit 1
 fi
 
-# Check brew doctor
-brew doctor
-
-# Install brew packages from Brewfile
-brew bundle install -v --file="$(pwd)"/Brewfile
-
-# Install oh-my-zsh
-# TODO oh-my-zsh install messes up the rest of the script
-# Figure out a way to install this in a separate terminal
-# chmod u+x "$(pwd)"/scripts/zsh_setup.sh
-# sh -c "$(pwd)"/scripts/zsh_setup.sh
-
-# Install fzf autocompletion and key bindings
-if test "$(which fzf)"; then
-    echo "Installing fzf autocomplete and key bindings..."
-    "$(brew --prefix)"/opt/fzf/install
-fi
-
-# Initialize miniconda
-conda init "$(basename "${SHELL}")"
-
-# Disable Microsoft AutoUpdate
-launchctl disable gui/"$(id -u)"/com.microsoft.update.agent
+exit 0
---
+++ install_linux.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "TBD"
+
+exit 0
