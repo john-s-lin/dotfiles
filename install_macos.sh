e22f597991d82169e5fd044366529c1ec2df1774
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2023-11-15 21:09:20 -0500

    feat: add linux support (#8)
    * feat: install.sh defers to OS-specific install scripts
    
    * feat: add macos install script
    
    * feat: init linux install script

---
+++ install_macos.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+if [[ "$OS" != "Darwin" ]]; then
+	echo "Oops! $OS is not MacOS. Running the wrong install script. Exiting..."
+	exit 1
+fi
+
+# Install Xcode CLT
+xcode-select --install
+
+# Install Homebrew
+echo "Checking for Homebrew..."
+if test ! "$(which brew)"; then
+	echo "Installing Homebrew..."
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	echo "Homebrew installed!"
+else
+	echo "$(brew --version) already installed."
+fi
+
+# Check brew doctor
+brew doctor
+
+# Install brew packages from Brewfile
+brew bundle install -v --file="$(pwd)"/Brewfile
+
+# Install oh-my-zsh
+# TODO oh-my-zsh install messes up the rest of the script
+# Figure out a way to install this in a separate terminal
+# chmod u+x "$(pwd)"/scripts/zsh_setup.sh
+# sh -c "$(pwd)"/scripts/zsh_setup.sh
+
+# Install fzf autocompletion and key bindings
+if test "$(which fzf)"; then
+	echo "Installing fzf autocomplete and key bindings..."
+	"$(brew --prefix)"/opt/fzf/install
+fi
+
+# Initialize miniconda
+conda init "$(basename "${SHELL}")"
+
+# Disable Microsoft AutoUpdate
+launchctl disable gui/"$(id -u)"/com.microsoft.update.agent
+
+exit 0
