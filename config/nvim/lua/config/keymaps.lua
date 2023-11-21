05508aa019d861415a55e9c4ce5d78478b54f669
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2023-11-21 18:44:05 -0500

    patch: remap "jj" to <ESC> in vim insert mode

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,4 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- Go to Explorer
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+-- Remap Esc key to "jj" in Insert mode
+vim.keymap.set("i", "jj", "<ESC>", { silent = true })
