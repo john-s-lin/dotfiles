f9a9378c084d64f517d93fc78eba83323e124081
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2024-07-23 17:03:34 -0400

    feat: remove symlink scripts from macos specific install script

--- scripts/macos_setup.sh
+++ scripts/macos_setup.sh
@@ -22,24 +22,6 @@ printf "\nSetting remote login to 'on'...\n"
 systemsetup -setremotelogin on
 systemsetup -getremotelogin
 
-parent_dir="$(dirname "$(pwd)")"
-
-# Enable symlinks for .zshrc, .bashrc, .condarc
-ln -sfn "${parent_dir}/.zshrc" "${HOME}/.zshrc"
-ln -sfn "${parent_dir}/.bashrc" "${HOME}/.bashrc"
-ln -sfn "${parent_dir}/.condarc" "${HOME}/.condarc"
-ln -sfn "${parent_dir}/.bash_aliases" "${HOME}/.bash_aliases"
-
-# Symlink config settings
-ln -sfn "${parent_dir}/config/nvim" "${HOME}/.config/nvim"
-ln -sfn "${parent_dir}/config/tmux" "${HOME}/.config/tmux"
-ln -sfn "${parent_dir}/config/alacritty" "${HOME}/.config/alacritty"
-
-# If bottom exists, symlink bottom.toml config
-if command -v btm >/dev/null 2>&1; then
-	ln -sfn "${parent_dir}/config/bottom" "${HOME}/.config/bottom"
-fi
-
 # REF: https://macos-defaults.com/
 # Set dock orientation to left and tilesize = 36
 defaults write com.apple.dock "tilesize" -int 36 &&
