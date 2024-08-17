dc6001292c784a93e849fdf172c5ead4d2a7a1a7
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2024-08-17 01:40:52 -0400

    feat: echo statements are more informative

--- scripts/conda_setup.sh
+++ scripts/conda_setup.sh
@@ -6,10 +6,11 @@ ARCH=$(uname -m)
 miniforge_dir="${HOME}/.miniforge3"
 
 if [[ -d "${miniforge_dir}" ]]; then
-    echo "Miniforge exists"
+    echo "miniforge exists"
     exit 1
 fi
 
+echo "Installing miniforge3..."
 mkdir -p "${miniforge_dir}"
 curl --output-dir "${miniforge_dir}" -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-${ARCH}.sh"
 bash "${miniforge_dir}/Miniforge3-$(uname)-${ARCH}.sh"
