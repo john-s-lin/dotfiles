aaed1fd689a9a50ec67c3daec927cf7879a94f6f
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2024-08-17 01:29:25 -0400

    feat: add conda setup script for linux

--- install_linux.sh
+++ install_linux.sh
@@ -1,5 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-echo "TBD"
+conda_setup_path="${DOTFILES_ROOT}/scripts/conda_setup.sh"
+chmod 755 "${conda_setup_path}"
+sh -c "${conda_setup_path}"
+chmod 644 "${conda_setup_path}"
 
 exit 0
