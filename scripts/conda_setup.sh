c16d12f0313b4fde095f75d4af636af113badeae
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2024-08-17 02:27:14 -0400

    feat: allow curl to follow links

--- scripts/conda_setup.sh
+++ scripts/conda_setup.sh
@@ -6,13 +6,13 @@ ARCH=$(uname -m)
 miniforge_dir="${HOME}/.miniforge3"
 
 if [[ -d "${miniforge_dir}" ]]; then
-    echo "miniforge exists"
-    exit 1
+	echo "miniforge exists"
+	exit 1
 fi
 
 echo "Installing miniforge3..."
 mkdir -p "${miniforge_dir}"
-curl --output-dir "${miniforge_dir}" -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-${ARCH}.sh"
+curl --output-dir "${miniforge_dir}" -LO "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-${ARCH}.sh"
 bash "${miniforge_dir}/Miniforge3-$(uname)-${ARCH}.sh"
 rm -rf "${miniforge_dir}/Miniforge3-$(uname)-${ARCH}.sh"
 
