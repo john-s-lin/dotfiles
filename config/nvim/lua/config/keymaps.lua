1283714351e3b679c41ff3b5bf5294e1b5d7209f
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2024-10-20 00:07:31 -0400

    feat: add 'kk' to keymap as alternatice esc seq

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -3,5 +3,6 @@
 -- Add any additional keymaps here
 -- Go to Explorer
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
--- Remap Esc key to "jj" in Insert mode
+-- Remap Esc key to "jj" and "kk" in Insert mode
 vim.keymap.set("i", "jj", "<ESC>", { silent = true })
+vim.keymap.set("i", "kk", "<ESC>", { silent = true })
