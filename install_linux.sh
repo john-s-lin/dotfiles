48f0665d18128853b95d523004ac2af2004f4001
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2025-01-01 12:58:00 -0500

    patch: no longer make conda necessary for setup in linux

--- install_linux.sh
+++ install_linux.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
 
-conda_setup_path="${DOTFILES_ROOT}/scripts/conda_setup.sh"
-chmod 755 "${conda_setup_path}"
-sh -c "${conda_setup_path}"
-chmod 644 "${conda_setup_path}"
-
 exit 0
