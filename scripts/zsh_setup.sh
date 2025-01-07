c83915828ac79aa6c89220e762b7ed7428501e91
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2025-01-07 14:57:02 -0500

    feat: update zsh setup script

--- scripts/zsh_setup.sh
+++ scripts/zsh_setup.sh
@@ -2,22 +2,37 @@
 
 # Ensure .oh-my-zsh is already installed
 if [[ -z "${ZSH}" || ! -d "${ZSH}" ]]; then
+	echo "Oh My Zsh not found. Installing..."
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+	# Reload Zsh environment
+	echo "Reloading Zsh environment..."
+	exec zsh
+else
+	echo "Oh My Zsh already installed."
 fi
 
-# Assert ZSH_CUSTOM exists
-if [ -z "${ZSH_CUSTOM}" ]; then
-	ZSH_CUSTOM="${ZSH}/custom"
-fi
-
-# Symlink modified passion theme to ZSH_CUSTOM
-ln -s "${HOME}/.dotfiles/config/zsh/themes/passion.zsh-theme" "${ZSH_CUSTOM}"/themes
-ln -s "${HOME}/.dotfiles/config/zsh/themes/robbyrussell.zsh-theme" "${ZSH_CUSTOM}"/themes
+# Symlink modified themes to ZSH_CUSTOM
+echo "Creating symlinks for custom themes..."
+ln -sf "${HOME}/.dotfiles/config/zsh/themes/passion.zsh-theme" "${ZSH_CUSTOM}/themes/passion.zsh-theme"
+ln -sf "${HOME}/.dotfiles/config/zsh/themes/robbyrussell.zsh-theme" "${ZSH_CUSTOM}/themes/robbyrussell.zsh-theme"
 
 # Download zsh-autosuggestions plugin
-git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM}"/plugins/zsh-autosuggestions
+if [ ! -d "${ZSH_CUSTOM}/plugins/zsh-autosuggestions" ]; then
+	echo "Installing zsh-autosuggestions plugin..."
+	git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM}/plugins/zsh-autosuggestions"
+else
+	echo "zsh-autosuggestions plugin already installed."
+fi
 
 # Download zsh-syntax-highlighting plugin
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM}"/plugins/zsh-syntax-highlighting
+if [ ! -d "${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting" ]; then
+	echo "Installing zsh-syntax-highlighting plugin..."
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting"
+else
+	echo "zsh-syntax-highlighting plugin already installed."
+fi
+
+echo "zsh setup complete."
 
 exit 0
