c044bc55b228e807e097b98cf3e1ff544bfd5437
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2026-08-16 17:48:31 -0700

    refactor(starship): use native TOML config

--- modules/home/starship.nix
+++ modules/home/starship.nix
@@ -1,47 +1,6 @@
 { ... }:
 {
-  programs.starship = {
-    enable = true;
-    settings = {
-      format = "$directory$python$rust$zig$git_branch$git_commit$git_state$git_status$cmd_duration$character";
-      add_newline = false;
+  programs.starship.enable = true;
 
-      directory = {
-        format = "[\\[$path\\]](\$style) ";
-        style = "cyan";
-        truncation_symbol = ".../";
-        truncate_to_repo = false;
-      };
-
-      cmd_duration = {
-        min_time = 50;
-        show_milliseconds = true;
-        style = "yellow";
-      };
-
-      python = {
-        format = "[\\((\$version)( \$virtualenv)\\)](\$style) ";
-        style = "green";
-      };
-
-      rust = {
-        format = "[\\((\$version)\\)](\$style) ";
-        style = "red";
-      };
-
-      zig = {
-        format = "[\\((\$version)\\)](\$style) ";
-        style = "yellow";
-      };
-
-      git_branch = {
-        format = "[\\[$branch(:\$remote_branch)\\]](\$style) ";
-        style = "purple";
-      };
-
-      git_status = {
-        style = "bold purple";
-      };
-    };
-  };
+  xdg.configFile."starship.toml".source = ./starship/starship.toml;
 }
